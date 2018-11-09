b8ace04a9618fc984d97fe216f3a8258e56fdfca
Felipe <phil.sousa@gmail.com>
2018-11-09 14:25:38 -0800

    Complete creating the grocery list component

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // TODO
+var GroceryList = () => (
+  <ul>
+    <li>Milk</li>
+    <li>apple</li>
+  </ul>
+);
+
+ReactDOM.render(<GroceryList/>, document.getElementById('app'));
+
+// Inside app.js, create a GroceryList component that contains an unordered list of 2 
+// grocery items. Render this component to the div tag in index.html with an id of app
+// Commit your progr
